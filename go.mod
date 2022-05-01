f744eb478572acfd10183be3001ab739e4a5df63
Les Orchard <me@lmorchard.com>
2022-04-30 22:55:48 -0700

    first hacked together feed fetcher and date-partitioned spooler

--- go.mod
+++ go.mod
@@ -3,20 +3,19 @@ module github.com/lmorchard/feedspool-go
 go 1.18
 
 require (
+	github.com/golang-queue/queue v0.1.1
 	github.com/mmcdole/gofeed v1.1.3
-	github.com/urfave/cli v1.22.8
+	github.com/sirupsen/logrus v1.8.1
 )
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
 	github.com/andybalholm/cascadia v1.1.0 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,87 +1,174 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"crypto/sha1"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+	"time"
+
+	"github.com/golang-queue/queue"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/mmcdole/gofeed"
-	"github.com/mmcdole/gofeed/atom"
-	"github.com/mmcdole/gofeed/rss"
-	"github.com/urfave/cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "ftest"
-	app.Usage = "provide a feed file path or url to parse and print"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "type,t",
-			Value: "universal",
-			Usage: "type of parser (atom, rss, universal)",
-		},
-	}
-	app.Action = func(c *cli.Context) {
-		if c.NArg() == 0 {
-			fmt.Println("Missing feed path or url")
-			os.Exit(1)
-		}
+	FEED_QUEUE_CONCURRENCY := 256
+	baseFeedsPath := "data/feeds"
 
-		feedType := c.String("type")
-		feedLoc := c.Args()[0]
+	mkdirpIfNotExist(baseFeedsPath)
 
-		fc, err := fetchFeed(feedLoc)
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		var feed interface{}
-
-		if strings.EqualFold(feedType, "rss") ||
-			strings.EqualFold(feedType, "r") {
-			p := rss.Parser{}
-			feed, err = p.Parse(strings.NewReader(fc))
-		} else if strings.EqualFold(feedType, "atom") ||
-			strings.EqualFold(feedType, "a") {
-			p := atom.Parser{}
-			feed, err = p.Parse(strings.NewReader(fc))
-		} else {
-			p := gofeed.NewParser()
-			feed, err = p.ParseString(fc)
-		}
-
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		fmt.Println(feed)
-	}
-	app.Run(os.Args)
-}
-
-func fetchFeed(feedLoc string) (string, error) {
-	if strings.HasPrefix(feedLoc, "http") {
-		return fetchURL(feedLoc)
-	}
-	file, err := fetchFile(feedLoc)
+	feedUrls, err := readLines("data/feeds.txt")
 	if err != nil {
-		return "", err
+		log.Fatal(err)
 	}
-	return string(file), nil
+
+	rets := make(chan string)
+	q := queue.NewPool(FEED_QUEUE_CONCURRENCY)
+	defer q.Release()
+
+	for i := 0; i < len(feedUrls); i++ {
+		go func(url string) {
+			log.Infof("enqueue: %s", url)
+
+			err := q.QueueTask(func(ctx context.Context) error {
+				err = pollOneFeed(url, baseFeedsPath)
+				rets <- url
+				return err
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(feedUrls[i])
+	}
+
+	// wait until all tasks done
+	for i := 0; i < len(feedUrls); i++ {
+		log.Infof("fetched: %s %s", i, <-rets)
+	}
 }
 
-func fetchFile(path string) (string, error) {
-	f, err := ioutil.ReadFile(path)
-	return string(f), err
+func readLines(fileName string) ([]string, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	lines := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+func pollOneFeed(url string, baseFeedsPath string) error {
+	hash := sha1Hash(url)
+	feedPath := fmt.Sprintf("%s/%s", baseFeedsPath, hash)
+	itemsPath := fmt.Sprintf("%s/items", feedPath)
+
+	mkdirpIfNotExist(feedPath)
+	mkdirpIfNotExist(itemsPath)
+
+	log.Infof("fetching: %s %s", hash, url)
+
+	fc, err := fetchURL(url)
+	if err != nil {
+		log.Warn(err.Error())
+		return nil
+	}
+
+	var feed *gofeed.Feed
+	feed, err = gofeed.NewParser().ParseString(fc)
+	if err != nil {
+		log.Warn(err.Error())
+		return nil
+	}
+
+	var itemsByDate = make(map[string][]*gofeed.Item)
+	for _, item := range feed.Items {
+		if item.PublishedParsed == nil {
+			log.Warnf("No published date for item in %s %s", url, hash)
+			continue
+		}
+		publishedDT := item.PublishedParsed.Format(time.RFC3339)
+		publishedParts := strings.Split(publishedDT, "T")
+		publishedDate := publishedParts[0]
+
+		items, exists := itemsByDate[publishedDate]
+		if exists {
+			itemsByDate[publishedDate] = append(items, item)
+		} else {
+			itemsByDate[publishedDate] = []*gofeed.Item{item}
+		}
+	}
+
+	for itemsKey, items := range itemsByDate {
+		itemsFilename := fmt.Sprintf("%s/%s.json", itemsPath, itemsKey)
+		err = writeJSONFile(itemsFilename, items)
+		if err != nil {
+			log.Warn(err.Error())
+			continue
+		}
+	}
+
+	feed.Items = []*gofeed.Item{}
+
+	metaFilename := fmt.Sprintf("%s/meta.json", feedPath)
+	err = writeJSONFile(metaFilename, feed)
+	if err != nil {
+		log.Warn(err.Error())
+		return nil
+	}
+
+	return nil
+}
+
+func writeJSONFile(fileName string, obj interface{}) error {
+	bytes, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(fileName, bytes, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func mkdirpIfNotExist(dirPath string) {
+	_, err := os.Stat(dirPath)
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(dirPath, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func sha1Hash(source string) string {
+	h := sha1.New()
+	h.Write([]byte(source))
+	bs := h.Sum(nil)
+	return fmt.Sprintf("%x", bs)
 }
 
 func fetchURL(url string) (string, error) {
-	response, err := http.Get(url)
+	client := http.Client{
+		Timeout: 3 * time.Second,
+	}
+	response, err := client.Get(url)
 	if err != nil {
 		return "", err
 	}
